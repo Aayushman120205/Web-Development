fb32799829f3923f72c5bdb8d9e02af7788cf8a2
Aayushman <143834698+Aayushman120205@users.noreply.github.com>
2025-06-04 01:14:04 -0600

    Add files via upload

---
+++ JavaScript/08_classes&oop/getter_setter.js
@@ -0,0 +1,54 @@
+class User1 {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh1 = new User1("h@hitesh.ai", "abc")
+console.log(hitesh.email);
+
+
+////////////////////////static property
+
+class User2 {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh2 = new User2("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
